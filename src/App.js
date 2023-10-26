a9a453e79bbc90736bbf2bf4b9a469e33604ef7b
pete08 <pete08@gmail.com>
2023-10-26 17:03:01 -0500

    Added stateful variables for contacts and appointments, sent props to contactsPage and appointmentsPage, set up useEffect hook to prevent duplicate contact names

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
-import React from "react";
-import { RouterProvider, createBrowserRouter, createRoutesFromElements, Route, Navigate } from "react-router-dom"
+import { React, useState } from "react";
+import { RouterProvider, createBrowserRouter, createRoutesFromElements, Route, Navigate } from "react-router-dom";
 import Root, { ROUTES } from "./components/root/Root";
 import { AppointmentsPage } from "./containers/appointmentsPage/AppointmentsPage";
 import { ContactsPage } from "./containers/contactsPage/ContactsPage";
@@ -9,17 +9,45 @@ function App() {
   Define state variables for 
   contacts and appointments 
   */
+  const [ contacts, setContacts ] = useState([]);
+  const [ appointments, setAppointments ] = useState([]);
 
   /*
   Implement functions to add data to
   contacts and appointments
   */
+  const handleContacts = ({ target }) => {
+    const name = target.name;
+    const phone = target.phone;
+    const email = target.email;
+    setContacts((prev) => {
+      return [...prev, {
+        "name": name,
+        "phone": phone,
+        "email": email}];
+    })
+  }
+
+  const handleAppointments = ({ target }) => {
+    const name = target.name;
+    const contact = target.contact;
+    const date = target.date;
+    const time = target.time;
+    setAppointments((prev) => {
+      return [...prev, {
+        "name": name,
+        "contact": contact,
+        "date": date,
+        "time": time
+      }];
+    })
+  }
 
   const router = createBrowserRouter(createRoutesFromElements(
     <Route path="/" element={ <Root/> }>
       <Route index element={ <Navigate to={ROUTES.CONTACTS} replace/> }/>
-      <Route path={ROUTES.CONTACTS} element={ <ContactsPage /> /* Add props to ContactsPage */ }/>
-      <Route path={ROUTES.APPOINTMENTS} element={ <AppointmentsPage /> /* Add props to AppointmentsPage */ }/>
+      <Route path={ROUTES.CONTACTS} element={ <ContactsPage contacts={contacts} contactCallback={handleContacts}/> /* Add props to ContactsPage */ }/>
+      <Route path={ROUTES.APPOINTMENTS} element={ <AppointmentsPage appointments={appointments} appointmentCallback={handleAppointments} /> /* Add props to AppointmentsPage */ }/>
     </Route>
   ));
   
--- src/containers/contactsPage/ContactsPage.js
+++ src/containers/contactsPage/ContactsPage.js
@@ -3,24 +3,39 @@ import React, { useState, useEffect } from "react";
 import { ContactForm } from "../../components/contactForm/ContactForm";
 import { TileList } from "../../components/tileList/TileList";
 
-export const ContactsPage = () => {
+export const ContactsPage = (props) => {
   /*
   Define state variables for 
   contact info and duplicate check
   */
+  const [name, setName] = useState("");
+  const [phone, setPhone] = useState("");
+  const [email, setEmail] = useState("");
+  const [dupeName, SetDupeName] = useState(false);
+
+
 
   const handleSubmit = (e) => {
-    e.preventDefault();
+    e.preventDefaul.t();
+    // 2023-10-27 START at #3 HINT: ...Within 'handleSubmit(), if the duplicate...
     /*
     Add contact info and clear data
     if the contact name is not a duplicate
     */
+    
+
   };
 
   /*
   Using hooks, check for contact name in the 
   contacts array variable in props
-  */
+  */ 
+  useEffect(() => {
+    const dupeNameFind = props.contacts.filter((contact) => contact.name === name)
+    if (dupeNameFind.length > 0) {
+      SetDupeName(true);
+    }
+  }, [name])
 
   return (
     <div>
